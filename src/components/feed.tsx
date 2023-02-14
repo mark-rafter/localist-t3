2f39722fa2379b7ceb02981140c924c0bccb1b56
mark-rafter <38855689+mark-rafter@users.noreply.github.com>
2023-02-14 21:02:59 +0000

    wip feed styling

--- src/components/feed.tsx
+++ src/components/feed.tsx
@@ -7,14 +7,16 @@ const FeedItem = ({
   postId,
   distance,
   price,
+  size,
 }: {
   postId: number;
   distance: number;
   price: number;
+  size: "XS" | "small" | "medium" | "large" | "XL" | "XXL";
 }) => {
   const tailwindSmPixels = 384;
   return (
-    <article className="max-w-sm rounded-lg border border-gray-200 bg-white antialiased shadow dark:border-gray-700 dark:bg-gray-800">
+    <article className="max-w-sm overflow-hidden rounded-lg border border-gray-200 bg-white antialiased shadow dark:border-gray-700 dark:bg-gray-800">
       <Link href={`post/${postId}`} prefetch={false}>
         <Image
           className="rounded-t-lg"
@@ -25,11 +27,13 @@ const FeedItem = ({
         />
       </Link>
       <div className="p-4">
+        <h3 className="text-sm uppercase tracking-wide text-gray-700 dark:text-gray-300">
+          {size}
+        </h3>
         <Link href={`post/${postId}`} prefetch={false}>
-          <h2 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-            Unisex plain purple tee
+          <h2 className="w-64 truncate text-xl font-bold tracking-tight text-gray-900 dark:text-white">
+            Unisex plain purple nylon tee, 3 years old
           </h2>
-          <h3 className="text-gray-700 dark:text-gray-300">Small</h3>
         </Link>
         <div className="flex items-center justify-between">
           <span className="text-2xl font-bold text-gray-900 dark:text-white">
@@ -57,7 +61,13 @@ export const Feed = () => (
     lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4"
   >
     {[...Array(8).keys()].map((k) => (
-      <FeedItem key={k + 1} postId={k + 1} price={33} distance={22} />
+      <FeedItem
+        key={k + 1}
+        postId={k + 1}
+        price={33}
+        distance={22}
+        size={"small"}
+      />
     ))}
   </div>
 );
