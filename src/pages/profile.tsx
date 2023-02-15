53493781bc1e418c7be7b51d977bf9c76e2a9120
mark-rafter <38855689+mark-rafter@users.noreply.github.com>
2023-02-15 19:42:59 +0000

    profile page details

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from "next";
 import { useSession } from "next-auth/react";
 import React from "react";
 import Head from "next/head";
+import Image from "next/image";
 
 interface Profile {
   name: string;
@@ -21,7 +22,18 @@ const Profile: NextPage = () => {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <div>{data?.user.id}</div>
       <div>{data?.user.name}</div>
+      <div>{data?.user.email}</div>
+      <div>{data?.user.image}</div>
+      {data?.user.image && (
+        <Image
+          src={data?.user.image}
+          width={250}
+          height={250}
+          alt="Profile picture"
+        />
+      )}
     </>
   );
 };
