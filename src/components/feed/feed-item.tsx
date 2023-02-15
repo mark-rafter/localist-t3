c8908794bd0cf07d3c8f7062bac076ecbbae2060
mark <38855689+mark-rafter@users.noreply.github.com>
2023-02-15 16:52:44 +0000

    rf feed-item

--- src/components/feed/feed-item.tsx
+++ src/components/feed/feed-item.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import Link from "next/link";
 import { HiOutlineClock, HiOutlineMapPin } from "react-icons/hi2";
 
-const FeedItem = ({
+export const FeedItem = ({
   postId,
   distance,
   price,
@@ -51,26 +51,3 @@ const FeedItem = ({
     </article>
   );
 };
-
-function getRandomInt(min: number, max: number) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
-}
-
-export const Feed = () => (
-  <div
-    className="mb-4 grid grid-cols-1 justify-items-center gap-4 rounded-lg p-4 
-    lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4"
-  >
-    {[...Array(8).keys()].map((k) => (
-      <FeedItem
-        key={k + 1}
-        postId={k + 1}
-        price={33}
-        distance={22}
-        size={"small"}
-      />
-    ))}
-  </div>
-);
