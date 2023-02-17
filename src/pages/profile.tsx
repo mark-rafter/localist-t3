923abdfde395bc6c056571ee86e433a565e66832
mark <38855689+mark-rafter@users.noreply.github.com>
2023-02-17 17:01:17 +0000

    Head cleanup

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -15,12 +15,6 @@ const Profile: NextPage = () => {
     <>
       <Head>
         <title>Your Profile | Localist</title>
-        <meta
-          name="description"
-          content="Localist - the free small advertising app"
-        />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
       </Head>
       <div>{data?.user.id}</div>
       <div>{data?.user.name}</div>
