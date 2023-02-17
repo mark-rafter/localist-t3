8229b0e372afdadefcff35ad6c73ce5b1919fe3f
mark-rafter <38855689+mark-rafter@users.noreply.github.com>
2023-02-17 19:54:26 +0000

    form-controller

---
+++ src/components/form/form-controller.tsx
@@ -0,0 +1,17 @@
+import type {
+  FieldErrors,
+  FieldValues,
+  UseFormRegister,
+} from "react-hook-form";
+
+type FormControllerProps<T extends FieldValues> = {
+  register: UseFormRegister<T>;
+  errors: FieldErrors<T>;
+} & React.PropsWithChildren;
+
+export const FormController = <T extends FieldValues>(
+  props: FormControllerProps<T>
+) => {
+  // todo: inject register and errors to each Form component
+  return <>{props.children}</>;
+};
