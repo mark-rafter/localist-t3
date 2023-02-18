fd777bc6652defa576d917fa775f4268fca017f4
mark-rafter <38855689+mark-rafter@users.noreply.github.com>
2023-02-18 20:47:58 +0000

    wip post cleanup

--- src/pages/api/post.ts
+++ src/pages/api/post.ts
@@ -1,3 +1,5 @@
+import { getServerAuthSession } from "@/server/auth";
+import { prisma } from "@/server/db";
 import type { NextApiRequest, NextApiResponse } from "next";
 import type { PostSchema } from "../newpost";
 import { postSchema } from "../newpost";
@@ -14,23 +16,41 @@ type ErrorResponse = {
   message: string;
 };
 
-export default function handler(
+const handler = async (
   req: PostApiRequest,
   res: NextApiResponse<PostResponse | ErrorResponse>
-) {
-  const { title, size, brand } = req.body;
-  if (req.method === "POST") {
-    const result = postSchema.safeParse(req.body);
-    if (!result.success) {
-      res.status(400).send({
-        message: result.error.message,
-      });
-    }
-    res.status(200).json({ message: `Title ${title}` });
-  }
-  // HTTP method not supported!
-  else {
+) => {
+  if (req.method !== "POST") {
     res.setHeader("Allow", ["POST"]);
     res.status(405).json({ message: `Method not supported.` });
+    return;
   }
-}
+
+  const session = await getServerAuthSession({ req, res });
+
+  if (!session) {
+    res.send({ message: "You are not authorised to post" });
+    return;
+  }
+
+  const parsedBody = postSchema.safeParse(req.body);
+
+  if (!parsedBody.success) {
+    res.status(400).send({
+      message: parsedBody.error.message,
+    });
+  }
+
+  res.status(200).json({ message: `Title ${req.body.title}` });
+  // const id = 0; //todo get from prisma
+  // const result = prisma.post.create({
+  //   data: {
+  //     ...parsedBody,
+  //     authorId: "12",
+  //   },
+  // });
+  // todo: return redirect to post/[id]
+  // res.redirect(307, `/post/${id}`);
+};
+
+export default handler;
