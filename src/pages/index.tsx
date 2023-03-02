6f1ce1f1e68da466593bdaa99c99bab0eca8de57
mark-rafter <38855689+mark-rafter@users.noreply.github.com>
2023-03-02 21:04:41 +0000

    simplify homepage DOM

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -37,42 +37,36 @@ export default function HomePage() {
       <Head>
         <title>Home | Localist</title>
       </Head>
-      <div className="container mx-auto max-w-md text-center">
-        <section>
-          <div className="mx-auto grid px-4 py-8">
-            <div className="mr-auto place-self-center">
-              <h1 className="mb-4 text-3xl font-extrabold leading-none tracking-tight md:text-6xl">
-                Welcome to Localist!
-              </h1>
-              <p className="mb-2 font-light text-gray-300 md:text-lg">
-                Localist is a free listing board, get started by browsing the
-                feed or posting your listing.
-              </p>
-              <HomePageLink
-                href="/feed"
-                gradientDuoTone="purpleToBlue"
-                icon={HiOutlineHome}
-              >
-                Browse the feed
-              </HomePageLink>
-              <HomePageLink
-                href="/newpost"
-                gradientDuoTone="greenToBlue"
-                icon={HiOutlinePencilSquare}
-              >
-                Post an item
-              </HomePageLink>
-              <HomePageLink
-                href="/settings"
-                gradientDuoTone="pinkToOrange"
-                icon={HiOutlineMapPin}
-              >
-                Set your location
-              </HomePageLink>
-            </div>
-          </div>
-        </section>
-      </div>
+      <section className="container mx-auto max-w-md px-4 py-8 text-center">
+        <h1 className="mb-4 text-3xl font-extrabold leading-none tracking-tight md:text-6xl">
+          Welcome to Localist!
+        </h1>
+        <p className="mb-2 font-light text-gray-300 md:text-lg">
+          Localist is a free listing board, get started by browsing the feed or
+          posting your listing.
+        </p>
+        <HomePageLink
+          href="/feed"
+          gradientDuoTone="purpleToBlue"
+          icon={HiOutlineHome}
+        >
+          Browse the feed
+        </HomePageLink>
+        <HomePageLink
+          href="/newpost"
+          gradientDuoTone="greenToBlue"
+          icon={HiOutlinePencilSquare}
+        >
+          Post an item
+        </HomePageLink>
+        <HomePageLink
+          href="/settings"
+          gradientDuoTone="pinkToOrange"
+          icon={HiOutlineMapPin}
+        >
+          Set your location
+        </HomePageLink>
+      </section>
     </>
   );
 }
