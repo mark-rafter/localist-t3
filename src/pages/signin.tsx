30deeb15600c2f346bc9807259bf66eebd85a2c0
mark <38855689+mark-rafter@users.noreply.github.com>
2023-03-08 12:23:15 +0000

    signing: more typesafety plus ux tweaks

--- src/pages/signin.tsx
+++ src/pages/signin.tsx
@@ -3,24 +3,30 @@ import type {
   GetServerSidePropsContext,
   InferGetServerSidePropsType,
 } from "next";
+import type { ClientSafeProvider } from "next-auth/react";
 import { getProviders, signIn } from "next-auth/react";
 import Head from "next/head";
+import Image from "next/image";
 import { FaGithub, FaSpotify } from "react-icons/fa";
 
 type ProviderStyle = { icon: JSX.Element; class: string };
 
-const providerStyles: Record<string, ProviderStyle> = {
-  github: {
-    icon: <FaGithub className="mr-2 h-5 w-5" />,
-    class:
-      "bg-[#24292F] hover:bg-[#050708]/30 focus:ring-[#24292F]/50 focus:ring-gray-500",
-  },
-  spotify: {
-    icon: <FaSpotify className="mr-2 h-5 w-5" />,
-    class:
-      "bg-[#1d9a49] hover:bg-[#050708]/30 focus:ring-[#1d9a49]/50 focus:ring-gray-500",
-  },
-};
+const providerStyles = new Map<ClientSafeProvider["id"], ProviderStyle>([
+  [
+    "github",
+    {
+      icon: <FaGithub className="mr-2 h-5 w-5" />,
+      class: "bg-[#24292F] hover:bg-[#050708]/30 focus:ring-[#24292F]/50",
+    },
+  ],
+  [
+    "spotify",
+    {
+      icon: <FaSpotify className="mr-2 h-5 w-5" />,
+      class: "bg-[#1d9a49] hover:bg-[#050708]/30 focus:ring-[#1d9a49]/50",
+    },
+  ],
+]);
 
 export default function SignInPage({
   providers,
@@ -31,27 +37,30 @@ export default function SignInPage({
         <title>Sign In | Localist</title>
         <meta name="description" content="Sign in to Localist" />
       </Head>
-      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#490141] to-[#20111e]">
-        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-          <h1 className="text-4xl font-extrabold tracking-tight sm:text-[5rem]">
-            Localist
-          </h1>
-          <div>
-            {Object.values(providers).map((provider) => (
-              <div key={provider.name}>
-                <button
-                  type="button"
-                  onClick={() => signIn(provider.id)}
-                  className={`mr-2 mb-2 inline-flex items-center rounded-lg px-5 py-2.5 text-center text-sm font-medium focus:outline-none focus:ring-4 ${
-                    providerStyles[provider.id]?.class ?? ""
-                  }`}
-                >
-                  {providerStyles[provider.id]?.icon}
+      <main className="container flex flex-col items-center md:pt-6">
+        <Image
+          src="/icon-512.png"
+          width={512 / 2}
+          height={512 / 2}
+          alt="Localist Logo"
+        />
+        <div className="mt-5">
+          {Object.values(providers).map((provider) => (
+            <div key={provider.name}>
+              <button
+                type="button"
+                onClick={() => signIn(provider.id)}
+                className={`mb-3 flex w-64 justify-center rounded-lg px-5 py-3 focus:outline-none focus:ring-4 focus:ring-gray-500 ${
+                  providerStyles.get(provider.id)?.class ?? ""
+                }`}
+              >
+                <div className="inline-flex items-center sm:text-sm">
+                  {providerStyles.get(provider.id)?.icon}
                   Sign in with {provider.name}
-                </button>
-              </div>
-            ))}
-          </div>
+                </div>
+              </button>
+            </div>
+          ))}
         </div>
       </main>
     </>
