09a75f892c94b3452a0ea4179ed72cec2ec5c40e
mark <38855689+mark-rafter@users.noreply.github.com>
2023-03-13 13:50:43 +0000

    add trpc user.updateCoords

---
+++ src/server/api/routers/user-router.ts
@@ -0,0 +1,25 @@
+import { createTRPCRouter, protectedProcedure } from "@/server/api/trpc";
+import { z } from "zod";
+
+export const userRouter = createTRPCRouter({
+  updateCoords: protectedProcedure
+    .input(
+      z.object({
+        lat: z.number(),
+        long: z.number(),
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      const updatedUser = await ctx.prisma.user.update({
+        where: {
+          id: ctx.session.user.id,
+        },
+        data: {
+          lat: input.lat,
+          long: input.long,
+        },
+      });
+
+      return updatedUser;
+    }),
+});
