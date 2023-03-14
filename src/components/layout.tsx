99573a7fd0c7b084da2cd19151cedd096a516cbb
mark <38855689+mark-rafter@users.noreply.github.com>
2023-03-14 14:37:07 +0000

    add react-hot-toast

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import { Toaster } from "react-hot-toast";
 import { Sidebar } from "./sidebar";
 
 export function Layout({ children }: React.PropsWithChildren) {
@@ -9,6 +10,15 @@ export function Layout({ children }: React.PropsWithChildren) {
         <meta name="description" content="Localist - the free listing app" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <Toaster
+        toastOptions={{
+          style: {
+            borderRadius: "10px",
+            background: "rgb(30 41 59)",
+            color: "#fff",
+          },
+        }}
+      />
       <Sidebar />
       <main className="p-2 sm:ml-56">{children}</main>
     </>
