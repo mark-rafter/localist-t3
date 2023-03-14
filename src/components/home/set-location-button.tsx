fc9c6eecfbe1a024c6e678920351403b831dd64d
mark <38855689+mark-rafter@users.noreply.github.com>
2023-03-14 14:37:59 +0000

    add toasts for set location btn

--- src/components/home/set-location-button.tsx
+++ src/components/home/set-location-button.tsx
@@ -1,6 +1,7 @@
 import { HomePageLink } from "@/components/home";
 import { api } from "@/utils/api";
 import { useEffect, useState } from "react";
+import toast from "react-hot-toast";
 import { HiOutlineMapPin } from "react-icons/hi2";
 
 export function SetLocationButton({
@@ -18,15 +19,31 @@ export function SetLocationButton({
   }, []);
 
   const setLocation = () => {
-    navigator.geolocation?.getCurrentPosition(
-      ({ coords }) => {
+    const getPositionPromise = new Promise(
+      (resolve: PositionCallback, reject: PositionErrorCallback) => {
+        navigator.geolocation?.getCurrentPosition(resolve, reject, {
+          timeout: 5000,
+          enableHighAccuracy: false,
+        });
+      }
+    );
+
+    toast
+      .promise(getPositionPromise, {
+        loading: "Updating location",
+        success: "Location updated!",
+        error: (err: PositionErrorCallback) =>
+          `Could not update location ${err.toString()}`,
+      })
+      .catch(console.error);
+
+    getPositionPromise.then(
+      ({ coords }) =>
         mutate({
           lat: coords.latitude,
           long: coords.longitude,
-        });
-      },
-      console.error,
-      { timeout: 5000, enableHighAccuracy: false }
+        }),
+      console.error
     );
   };
 
