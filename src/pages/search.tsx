f658084dd821f964af636294ada6d9fda9df746d
mark <38855689+mark-rafter@users.noreply.github.com>
2023-03-15 19:31:14 +0000

    cleanup search

--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -1,5 +1,6 @@
 import { ClientFeed, LoadMore } from "@/components/feed";
 import { api } from "@/utils/api";
+import { useAutoAnimate } from "@formkit/auto-animate/react";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { TextInput } from "flowbite-react";
 import Head from "next/head";
@@ -10,9 +11,10 @@ import InfiniteScroll from "react-infinite-scroll-component";
 import { z } from "zod";
 
 const postsPerPage = 8;
+const searchMaxLength = 32;
 
 const searchSchema = z.object({
-  q: z.string().max(25, { message: "Query too long" }).default(""),
+  q: z.string().max(searchMaxLength, { message: "Query too long" }).default(""),
 });
 
 export type SearchSchema = z.infer<typeof searchSchema>;
@@ -26,6 +28,7 @@ export function SearchForm() {
     resolver: zodResolver(searchSchema),
   });
   const router = useRouter();
+  const [parent] = useAutoAnimate();
 
   const submitForm = handleSubmit(
     async (formData) =>
@@ -40,23 +43,26 @@ export function SearchForm() {
   );
 
   return (
-    <div className="relative mx-auto w-96">
-      <form onSubmit={submitForm}>
-        <TextInput
-          id="search"
-          {...register("q")}
-          maxLength={32}
-          icon={HiMagnifyingGlass}
-          placeholder="e.g. womens nike trainers size 10"
-        />
-        <button
-          type="submit"
-          className="absolute top-0 right-0 p-2 text-md font-medium text-white bg-blue-700 rounded-r-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-        >
-          Go
-        </button>
-      </form>
-    </div>
+    <form className="mx-auto w-80 sm:w-96" ref={parent} onSubmit={submitForm}>
+      <TextInput
+        id="search"
+        {...register("q")}
+        maxLength={searchMaxLength}
+        icon={HiMagnifyingGlass}
+        placeholder="e.g. womens nike trainers size 10"
+      />
+      <button
+        type="submit"
+        className="absolute top-0 right-0 p-2 text-md font-medium text-white bg-blue-700 rounded-r-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+      >
+        Go
+      </button>
+      {errors.q && (
+        <p id="q_error_message" className="mt-2 text-xs text-red-400">
+          {errors.q.message}
+        </p>
+      )}
+    </form>
   );
 }
 function useSearchTerm() {
