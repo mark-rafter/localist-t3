f871718bad5c49c74845aab113e6ff70eb30c257
mark <38855689+mark-rafter@users.noreply.github.com>
2023-04-22 19:49:38 +0100

    wip: seed ratings

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -7,11 +7,13 @@ const prisma = new PrismaClient();
 const USERS_TO_CREATE = 15;
 const POSTS_MIN = 1;
 const POSTS_MAX = 6;
+const RATINGS_MIN = 4;
+const RATINGS_MAX = 12;
 
 async function run() {
-  const users = await createUsers();
-  const userIds = users.map((u) => u.id);
-  await createPosts(userIds);
+  const createdUserIds = await createUsers();
+  await createUserRatings(createdUserIds);
+  await createPosts(createdUserIds);
 
   await prisma.$disconnect();
 }
@@ -43,6 +45,37 @@ async function createPosts(userIds: string[]) {
   await prisma.$transaction(createPosts);
 }
 
+async function createUserRatings(userIds: string[]) {
+  const generateFakeRatingsByUser = (userIds: string[], byUserId: string) => {
+    const generateFakeRating = (otherUserIds: string[], byUserId: string) =>
+      ({
+        score: faker.number.int({ min: 0, max: 5 }),
+        ratedByUser: {
+          connect: { id: byUserId },
+        },
+        ratingForUser: {
+          connect: { id: faker.helpers.arrayElement(otherUserIds) },
+        },
+      } as Prisma.UserRatingCreateInput);
+
+    const amount = faker.number.int({ min: RATINGS_MIN, max: RATINGS_MAX });
+    const otherUserIds = userIds.filter((id) => id === byUserId);
+    return Array(amount)
+      .fill(null)
+      .map(() => generateFakeRating(otherUserIds, byUserId));
+  };
+
+  const ratingData = userIds.flatMap((userId) =>
+    generateFakeRatingsByUser(userIds, userId)
+  );
+
+  const createRatings = ratingData.map((rating) =>
+    prisma.userRating.create({ data: rating })
+  );
+
+  await prisma.$transaction(createRatings);
+}
+
 async function createUsers() {
   const userData = Array(USERS_TO_CREATE).fill(null).map(generateFakeUser);
 
@@ -51,5 +84,5 @@ async function createUsers() {
   );
 
   const users = await prisma.$transaction(createUsers);
-  return users;
+  return users.map((u) => u.id);
 }
