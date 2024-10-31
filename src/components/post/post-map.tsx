87ae69f7480eb023e1bc47c1e58e2e1ffd9b0b71
mark-rafter <38855689+mark-rafter@users.noreply.github.com>
2024-10-31 13:13:28 +0000

    remove leaflet attribution

--- src/components/post/post-map.tsx
+++ src/components/post/post-map.tsx
@@ -9,14 +9,12 @@ export function PostMap({ lat, long }: Coordinates) {
     <div>
       <MapContainer
         center={[lat, long]}
+        attributionControl={false}
         zoom={10}
         scrollWheelZoom={false}
         style={{ width: "300px", height: "200px" }}
       >
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
+        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
         <Marker position={[lat, long]} />
       </MapContainer>
     </div>
